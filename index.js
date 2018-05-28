7d744b08f588ac73bed24eb6844e30e55e599461
YAANNZ <zhuynchn@163.com>
2018-05-28 23:34:34 +0800

    home.js

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express')
 const app = express()
 const indexRouter = require('./routes/index')
 const homeRouter = require('./routes/home')
+const optionsRouter = require('./routes/options')
 
 app.use('/', indexRouter)
 app.use('/home', homeRouter)
+app.use('/options', optionsRouter)
+
+app.use(function (req, res) {
+	if (!res.headerSent) {
+		res.writeHead(404, {"Content-Type":"text/html;charset=UTf-8"})
+		res.end('some wrong')
+	}
+}
 
 app.listen(3000)
 
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
 
-router.get('/', function (req, res) {
-	res.send('hello, this is home')
+router.get('/allTasks', function (req, res) {
+	fs.readFile('./tempData/allTasks.json', function(err, data){
+		res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'})
+		res.end(data)
+	})
 })
 
 module.exports = router
